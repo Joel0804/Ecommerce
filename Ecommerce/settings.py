e3684f47d081d432aafb566a09babe2469fb7f28
Joel Dsilva <joeldsilva0804@gmail.com>
2026-05-15 11:55:41 +0530

    settings

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -106,6 +106,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 STATIC_URL = 'static/'
 STATICFILES_DIRS = ['static/']
 
